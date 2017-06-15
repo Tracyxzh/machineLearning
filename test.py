bb435440e823cc9172611ecac34e496e5b04a3be
Tracyxzh <243307483@qq.com>
2017-06-15 22:50:40 +0800

    逻辑回归

---
+++ test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+#coding:utf-8
+
+# from numpy import *
+# frTrain = open('./Logistic/horseColicTraining.txt')
+# trainingSet = []
+# trainingLabels = []
+# for line in frTrain.readlines():
+# 	currLine = line.strip().split('\t')
+# 	lineArr = []
+# 	for i in range(21):
+# 		lineArr.append(float(currLine[i]))
+# 	trainingSet.append(lineArr)
+# 	trainingSet.append(float(currLine[21]))
+# print trainingSet
+
+print [0] * 3
